f8fa30cf64724e6361ac8fbc404e714161f08049
omar jakmira <poweromar19942@gmail.com>
2020-09-12 20:40:44 +0300

    add mongoose and create user model

---
+++ auth/src/models/user.ts
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose'
+import { Password } from '../services/password'
+
+// An interface that describes the properties
+// that are requried to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password',hashed)
+  }
+  done()
+})
+
+userSchema.statics.build = function (user: UserAttrs) {
+  return new User(user)
+}
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
+
+export { User }
+
+User.build({ password: '', email: '' })
