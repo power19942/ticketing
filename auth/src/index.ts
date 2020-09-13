6032c5581e35feaeb52ee222696a3f53e41a1866
omar jakmira <poweromar19942@gmail.com>
2020-09-13 13:54:32 +0300

    add auth test

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,33 +1,5 @@
-import express from 'express';
-import 'express-async-errors'
 import mongoose from 'mongoose'
-import cookieSession from 'cookie-session'
-
-import { CurrentUserRouter } from './routes/current-user'
-import { SignInRouter } from './routes/signin'
-import { SignUpRouter } from './routes/signup'
-import { SignOutRouter } from './routes/signout'
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express()
-app.set('trust proxy', true)
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieSession({
-  signed: false,
-  // secure: true
-}))
-
-app.use(CurrentUserRouter)
-app.use(SignInRouter)
-app.use(SignUpRouter)
-app.use(SignOutRouter)
-
-app.all('*', async () => {
-  throw new NotFoundError()
-})
-app.use(errorHandler)
+import {app} from './app'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
